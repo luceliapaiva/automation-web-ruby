8369fbd2ecb0e9b884f5a06cc16a30da56733f90
luceliapaiva <paivaluce@gmail.com>
2022-07-14 17:12:00 -0300

    Config environment, cucumber file yaml

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,11 +1,16 @@
 require 'capybara/cucumber'
 require 'capybara/rspec'
 require 'selenium-webdriver'
-require 'site_prism'
+require 'site_prism' 
 require 'pry'
+
+ENVIRONMENT = ENV['ENVIRONMENT']
+puts "ENVIRONMENT in runner >>> #{ENVIRONMENT}"
+
+CONFIG = YAML.load_file(File.dirname(__FILE__) + "environment/#{ENVIRONMENT}.yml")
 
 Capybara.configure do |config|
 config.default_driver = :selenium_chrome
-config.app_host = "http://www.google.com.br"
+config.app_host = CONFIG['url_default']
 config.default_max_wait_time = 10
 end
