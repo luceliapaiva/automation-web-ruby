709c191d675b253028c373293b91cd371be6554b
luceliapaiva <paivaluce@gmail.com>
2022-07-14 16:34:46 -0300

    Config hooks,helper file

---
+++ features/support/hooks.rb
@@ -0,0 +1,14 @@
+Before do |scenario|
+    Capybara.current_session.driver.manager.delete_all_cookies
+    page.driver.quit
+end
+
+After do |scenario|
+    scenario_name = scenario.name.gsub(/\s+/, '_').tr('/','_')
+
+    if scenario.failed?
+        print_screen(scenario_name.downcase!, 'Failed')
+    else
+        print_screen(scenario_name.downcase!, 'Sucess')
+    end
+end
