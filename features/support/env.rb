0eb647a4c75049c55cae8cacab2856e7c98691d8
luceliapaiva <paivaluce@gmail.com>
2022-07-18 14:28:21 -0300

    Add file page_helper

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,11 +3,15 @@ require 'capybara/rspec'
 require 'selenium-webdriver'
 require 'site_prism' 
 require 'pry'
+require_relative 'helper.rb'
+require_relative 'page_helper.rb'
 
 ENVIRONMENT = ENV['ENVIRONMENT']
 puts "ENVIRONMENT in runner >>> #{ENVIRONMENT}"
 
-CONFIG = YAML.load_file(File.dirname(__FILE__) + "environment/#{ENVIRONMENT}.yml")
+CONFIG = YAML.load_file(File.dirname(__FILE__) + "environment/#{/ENVIRONMENT}.yml")
+World(Helper)
+World(Pages)
 
 Capybara.configure do |config|
 config.default_driver = :selenium_chrome
---
+++ features/support/page_helpers.rb
@@ -0,0 +1,13 @@
+Dir[File.join(File.dirname(__FILE__), "../pages/*_page.rb")].each {|file| require file}
+
+module Pages 
+
+def home
+    @home ||= Home.new
+end
+
+def login 
+    @login ||= Login.new
+end
+
+end
